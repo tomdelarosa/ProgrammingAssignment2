c627f0909f426904d9956e3e9f5a2fa8682a9b89
Tomas de la Rosa <tomdelarosa@gmail.com>
2015-02-19 23:14:35 +0100

    R assigment raw code

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,28 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+    invcached <- NULL
+
+    set <- function(y) {
+        x <<- y
+        invcached <<- NULL
+    }
+
+    get <- function(){
+        x
+    }
+
+    setinv <- function(inverse){
+            invcached <<- inverse
+    }
+    
+    getinv <- function(){
+        invcached
+    }
+
+    list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 
 }
 
@@ -12,4 +34,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+     inv <- x$getinv()
+        if(!is.null(inv)) {
+                #
+                return(inv)
+        }
+        matrixdata <- x$get()
+        inv <- solve(matrixdata)
+        x$setinv(inv)
+        inv
+
 }
